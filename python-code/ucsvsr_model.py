9ef981396751460445400b9cffdcc51de8f71e97
Fabchirajoul <fabchirajoul@gmail.com>
2023-09-21 15:25:11 +0200

    changed package file

--- python-code/ucsvsr_model.py
+++ python-code/ucsvsr_model.py
@@ -1,10 +1,13 @@
-import sklearn
 import pickle
 import sys
-# import { scikit-learn }
+
 
 
 
 with open('models/ucsvsr_update.pkl','rb') as f:
         ucsvsr_model = pickle.load(f)
         print(ucsvsr_model.predict([[sys.argv[1], sys.argv[2], sys.argv[3]]]))
+
+
+
+
