87a0c3a8c7013b6039ffe3c62229658b41ff2ad8
Fabchirajoul <fabchirajoul@gmail.com>
2023-09-21 10:56:19 +0200

    upgraded the version of sklearn in jupiter notebook and resaved one model

--- python-code/ucsvsr_model.py
+++ python-code/ucsvsr_model.py
@@ -3,6 +3,6 @@ import sys
 # import  sklearn
 
 
-with open('models/ucsvsr.pkl','rb') as f:
+with open('models/ucsvsr_update.pkl','rb') as f:
         ucsvsr_model = pickle.load(f)
         print(ucsvsr_model.predict([[sys.argv[1], sys.argv[2], sys.argv[3]]]))
